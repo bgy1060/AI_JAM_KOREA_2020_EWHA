386978cc8fe694a5b2a42e57abb194f1c96d5ba5
Jaehyang <67912579+Jaehyang@users.noreply.github.com>
2020-07-13 20:28:37 +0900

    Create Okt_noun_content.py

---
+++ Tokenizer/Okt_noun_content.py
@@ -0,0 +1,26 @@
+'''
+Twitter(Okt) - 오픈소스 한글 형태소 분석기
+'''
+
+import pandas as pd
+from konlpy.tag import Okt
+import nltk
+
+data = pd.read_excel('petition.xlsx')[['제목', '본문']]
+okt = Okt()
+
+# 콘텐트 토큰에서 개수 추출
+for content in data['본문']:
+    content_token = okt.nouns(content)
+    print("본문에서 추출한 명사:", content_token)
+
+    text = nltk.Text(content_token)
+
+    # 전체 토큰의 개수
+    print("전체 토큰의 개수: ", len(content_token))
+    # 중복을 제외한 토큰의 개수
+    print("중복을 제외한 토큰의 개수:", len(set(content_token)))
+    # 출현 빈도가 높은 상위 토큰 20개
+    print("출현 빈도가 높은 상위 토큰 20개: ", text.vocab().most_common(20))
+    print("-"*60)
+
