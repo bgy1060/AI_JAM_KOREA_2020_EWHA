5ff3242f996f88503c07159e3ceb118292ca4a7e
Jaehyang <67912579+Jaehyang@users.noreply.github.com>
2020-07-16 15:31:54 +0900

    Update Okt_noun.py

--- Tokenizer/Okt_noun.py
+++ Tokenizer/Okt_noun.py
@@ -4,10 +4,7 @@ Twitter(Okt) - 오픈소스 한글 형태소 분석기
 
 import pandas as pd
 from konlpy.tag import Okt
-
 data = pd.read_excel('petition.xlsx')[['제목', '본문']]
-# print(data.head())
-
 okt = Okt()
 
 # 타이틀 토큰
@@ -22,5 +19,8 @@ print("<본문에서 추출한 명사>")
 for content in data['본문']:
     content_token = okt.nouns(content)
     print(content_token)
+
+
+
 
 
