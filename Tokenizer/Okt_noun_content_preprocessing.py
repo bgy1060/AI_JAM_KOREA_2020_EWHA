a44c1d4836585e7f920c7ee70eaf1e4c877f8228
Jaehyang <67912579+Jaehyang@users.noreply.github.com>
2020-07-16 15:32:03 +0900

    Update Okt_noun_content_preprocessing.py

--- Tokenizer/Okt_noun_content_preprocessing.py
+++ Tokenizer/Okt_noun_content_preprocessing.py
@@ -10,25 +10,24 @@ data = pd.read_excel('petition.xlsx')[['제목', '본문']]
 okt = Okt()
 
 # 1. 토큰화
-
 for i in range(data['제목'].count()):
-    # 제목 토큰
-    title_token = okt.nouns(data['제목'][i])
-    # 콘텐트 토큰
-    content_token = okt.nouns(data['본문'][i])
+    title_token = okt.nouns(data['제목'][i])      # 제목 토큰
+    content_token = okt.nouns(data['본문'][i])    # 콘텐트 토큰
     text = nltk.Text(content_token)
-    # 출현 빈도가 높은 본문 상위 토큰 15개
-    selected_words = [f[0] for f in text.vocab().most_common(15)]  # ('단어', '빈도수') 중 단어만
+    selected_words = [f[0] for f in text.vocab().most_common(15)]  # 출현 빈도가 높은 본문 상위 토큰 15개: ('단어', '빈도수') 중 단어만
 
 # 2. 제목이랑 본문상위토큰 합치기
     hap_token = title_token + selected_words
     print("제목 토큰 & 본문에서 가장 많이 나온 명사 15개: \n", hap_token)
 
-# 3. 대명사 및 조사 등 제외하기
-    delete_word = ['이', '그', '저', '것', '제', '저희', '너', '곳', '등', '더', '더욱', '후', '때']
+# 3. 불용어 제외하기
+    delete_word = ['이', '그', '그녀', '저', '것', '들', '제', '저희', '너', '되', '수', '않', '없', '아니',
+                   '때문', '곳', '등', '들', '중', '좀', '잘', '더', '더욱', '경우', '후', '때', '있', '하', ]
     final_token = []
     for word in hap_token:
         if word not in delete_word:
             final_token.append(word)
     print("대명사 등 의미 없는 단어 제거한 최종 토큰: \n", final_token)
     print("-" * 50)
+
+
